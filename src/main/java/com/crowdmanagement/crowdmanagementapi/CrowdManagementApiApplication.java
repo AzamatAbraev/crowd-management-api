bf587f3fc95231fa6de6196443394232a5f74713
AzamatAbraev <azamatabraev07@gmail.com>
2026-01-04 21:39:22 +0500

    Api initialized and demo controller tested

---
+++ src/main/java/com/crowdmanagement/crowdmanagementapi/CrowdManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.crowdmanagement.crowdmanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrowdManagementApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrowdManagementApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/crowdmanagement/crowdmanagementapi/CrowdManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crowdmanagement.crowdmanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrowdManagementApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
